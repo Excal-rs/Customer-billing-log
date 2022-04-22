d32a31a03fe39f34c2239f76dd1e852719be2a24
excal.rs <91800528+Excal-rs@users.noreply.github.com>
2022-04-22 12:00:14 +0100

    Create customer_billing_log.c

---
+++ customer_billing_log.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+char shop_name[] = "Excals shop";
+// functions to create bill
+void BillHeader(char name[50], char date[10]){
+	char name[50];
+	char date[10];
+	printf("\n\n");
+	printf("\t	%s", shop_name);
+	printf("\n-----------------------------------------------");
+	printf("\nDate: %s", date);
+	printf("\nInvoice to: %s \n", name);
+	printf("-----------------------------------------------\n");
+	printf("Iteams:\t\t");
+	printf("Qty\t\t");
+	printf("Total\t\t");
+	printf("Total\t\t");
+	printf("\n-----------------------------------------------");
+	printf("\n\n");
+}
+
+void BillBody(char itea)
+int main(){
+
+}
